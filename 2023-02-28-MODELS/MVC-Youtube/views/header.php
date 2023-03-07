fd2f33c702fe04091a41364f9d52fac9e558922a
GretaStaseliene <gretatamasauskait3@gmail.com>
2023-03-07 19:19:25 +0200

    header added

---
+++ 2023-02-28-MODELS/MVC-Youtube/views/header.php
@@ -0,0 +1,24 @@
+<div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <a href="./" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <img src="https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500" alt="youtube logo" class="logo">
+        </a>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <form class="w-100 me-3 d-flex" role="search">
+                <input type="hidden" name="page" value="search">
+                <input type="search" class="form-control rounded-end rounded-pill" placeholder="Search..." aria-label="Search" style="width: 400px;" name="search">
+                <button class="btn btn-outline-dark border border-start-0 rounded-start rounded-pill" type="button" id="button-addon2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                    </svg>
+                </button>
+            </form>
+        </ul>
+
+        <div class="col-md-3 text-end">
+            <a href="#" class="btn btn-outline-dark me-2">Login</a>
+            <a href="#" class="btn btn-dark">Sign-up</a>
+        </div>
+    </header>
+</div>
