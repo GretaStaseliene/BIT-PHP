ae4b8a877782d39c3f47ff9f9a9f76a2c2df1b5e
GretaStaseliene <gretatamasauskait3@gmail.com>
2023-03-13 17:09:32 +0200

    comments section added, according if user loged in or not

--- 2023-02-28-MODELS/MVC-Youtube/views/singleVideo.php
+++ 2023-02-28-MODELS/MVC-Youtube/views/singleVideo.php
@@ -1,5 +1,5 @@
-<?php 
-    include __DIR__ . '/partials/header.php';
+<?php
+include __DIR__ . '/partials/header.php';
 ?>
 
 <div class="container">
@@ -9,8 +9,33 @@
         </div>
         <div class="fs-4"><?= $video['name'] ?></div>
     <?php endforeach; ?>
+
+    <div class="comments mt-3">
+        <?php if (isset($_SESSION['user_id'])) : ?>
+            <form method="POST">
+                <div class="mb-3">
+                    <span class="p-2 mb-3 bg-success text-white rounded-pill"><?= $_SESSION['user_name'] ?></span>
+                    <label class="form-label">Leave your comment here</label>
+                    <textarea class="form-control mt-2" name="comment" cols="30" rows="5"></textarea>
+                    <button class="btn btn-outline-dark mt-3">Comment</button>
+                </div>
+            </form>
+        <?php else : ?>
+            <form method="POST">
+                <div class="mb-3">
+                    <label class="form-label">Your name</label>
+                    <input class="form-control" type="text" name="guest_name">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Leave your comment here</label>
+                    <textarea class="form-control" name="comment" cols="30" rows="5"></textarea>
+                    <button class="btn btn-outline-dark mt-3">Comment</button>
+                </div>
+            </form>
+        <?php endif; ?>
+    </div>
 </div>
 
 <?php
-    include __DIR__ . '/partials/footer.php';
+include __DIR__ . '/partials/footer.php';
 ?>
